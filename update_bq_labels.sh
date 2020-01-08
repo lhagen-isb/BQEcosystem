96793a4e983936fb487c51ce0c572110d73b2837
Kawther <40669059+kabdilleh1@users.noreply.github.com>
2020-01-08 12:55:09 -0500

    Update update_bq_labels.sh

--- update_bq_labels.sh
+++ update_bq_labels.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash                                                                                                                                                                  
 echo $BASH_VERSION
 
-     project="pancancer-atlas"
+     project="isb-cgc"
 
      declare -A ary
      
      while IFS="=" read -r key value; do
          ary[$key]=$value
-     done < "DataTypeLabels-PanCan.txt"
+     done < "SourceLabels.txt"
 
     for k in "${!ary[@]}"; do
       bq update --set_label "${ary[$k]}" "$project:$k"
